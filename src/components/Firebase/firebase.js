122769a60582f25b3b1f549f755dcc7075529228
u180189 <kyla.adanza.2017@mumail.ie>
2019-10-14 14:17:23 +0100

    Fixed sign in with GMail

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/firestore';
+
 
 
 var config = {
@@ -17,6 +19,7 @@ class Firebase {
   constructor() {
     app.initializeApp(config);
     this.auth = app.auth();
+    this.db = app.firestore();
 
     this.googleProvider = new app.auth.GoogleAuthProvider();
 
@@ -31,14 +34,9 @@ class Firebase {
     this.auth.onAuthStateChanged(authUser => {
       if (authUser) {
         this.user(authUser.uid)
-          .once('value')
+          .get()
           .then(snapshot => {
-            const dbUser = snapshot.val();
-
-            // default empty roles
-            if (!dbUser.roles) {
-              dbUser.roles = {};
-            }
+            const dbUser = snapshot.data();
 
             // merge auth and db user
             authUser = {
@@ -58,8 +56,8 @@ class Firebase {
 
     // *** User API ***
 
-    user = uid => this.db.ref(`users/${uid}`);
+    user = uid => this.db.doc(`users/${uid}`);
 
-    users = () => this.db.ref('users');
+    users = () => this.db.collection('users');
 }
 export default Firebase;
