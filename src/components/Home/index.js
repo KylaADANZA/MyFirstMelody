bc9e9e6c2a3b5b2e3323c31bfcffd0fb8019acc8
unknown <tzirath.perez@hotmail.com>
2019-10-23 15:24:55 +0100

    added new pic and changed home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import * as ROUTES from '../../constants/routes';
 import {Link} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -9,25 +10,25 @@ const HomePage = () => (
 		<Row>
 			<Col></Col>
 			<Col lg="auto" md="auto" sm="auto" className="heading">
-				<h1>Play Now!</h1>
+				<h1>My First Melody</h1>
 			</Col>
 			<Col></Col>
 		</Row>
 		<Row className="align-items-center justify-items-center">
 			<Col>
-				<Link to={ROUTES.GAME1}><Image STYLE="position:absolute; TOP:35px; LEFT:0px; WIDTH:350px; HEIGHT:300px" src="
+				<Link to={ROUTES.GAME1}><Image className="gameImage" STYLE="position:absolute; TOP:35px; LEFT:0px; WIDTH:350px; HEIGHT:300px" src="
 				images/tapTune.png"></Image></Link>
 			</Col>
 		</Row>
 		<Row className="align-items-center justify-items-center">
 			<Col>
-				<Link to={ROUTES.GAME2}><Image STYLE="position:absolute; TOP:35px; LEFT:400px; WIDTH:350px; HEIGHT:300px" src="
+				<Link to={ROUTES.GAME2}><Image className="gameImage" STYLE="position:absolute; TOP:35px; LEFT:400px; WIDTH:350px; HEIGHT:300px" src="
 				images/musicalMemory.png"></Image></Link>
 			</Col>
 		</Row>
 		<Row className="align-items-center justify-items-center">
 			<Col>
-				<Link to={ROUTES.GAME3}><Image STYLE="position:absolute; TOP:35px; LEFT:800px; WIDTH:350px; HEIGHT:300px" src="
+				<Link to={ROUTES.GAME3}><Image className="gameImage" STYLE="position:absolute; TOP:35px; LEFT:800px; WIDTH:350px; HEIGHT:300px" src="
 				images/funkyNotes.png"></Image></Link>
 			</Col>
 		</Row>
